65a91765ef7f88f966be05a3136ed9bb7bd38225
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-04-09 22:48:49 +0300

    Updates

---
+++ src/02.arrays.js
@@ -0,0 +1,139 @@
+/**
+ *  Arrays
+ */
+
+
+// array literal
+var arr = [1, 2];
+// var arr2 = new Array(2);
+
+
+/**
+ *  element access
+ */
+
+// define higher index element
+// arr[5] = 5;
+
+
+/**
+ *  length
+ */
+
+arr['a'] = 5;
+console.log( arr.length );
+
+
+/**
+ *  manipulation
+ */
+
+// https://graelaws.files.wordpress.com/2010/08/capture1.jpg
+// http://i.stack.imgur.com/1pQk8.jpg
+
+
+arr.push(3);
+arr.push(4);
+// arr.unshift(0);
+// console.log( arr.length );
+
+
+/**
+ *  stack (modals on top of each other)
+ */
+
+// console.log( arr );
+// console.log( arr.pop() );
+// console.log( arr.pop() );
+// console.log( arr );
+
+
+/**
+ *  queue (process queue, image processing)
+ */
+
+// console.log( arr );
+// console.log( arr.shift() );
+// console.log( arr.shift() );
+// console.log( arr );
+
+
+/**
+ *  mixed
+ */
+
+// console.log( arr );
+// console.log( arr.pop() );
+// console.log( arr.shift() );
+// console.log( arr );
+
+
+/**
+ *  sub-arrays
+ */
+
+// console.log( arr.slice(1, 3) );
+// console.log( arr.splice(1, 2, 5, 6) );
+// console.log( arr );
+
+
+/**
+ *  find index
+ */
+// console.log( arr.indexOf(1) );
+
+
+
+/**
+ *  filter
+ */
+
+var odds = arr.filter(function(item) {
+	return (item % 2) !== 0;
+});
+// console.log(odds);
+
+
+/**
+ *  iterations
+ */
+
+for (var i = 0; i < arr.length; i += 1) {
+	// console.log(arr[i], i);
+	arr[i] += 10;
+}
+// console.log( arr );
+
+arr.forEach(function(item, index) {
+	// console.log(item, index);
+	arr[i] += 10;
+});
+// console.log( arr );
+
+var arr2 = arr.map(function(item, index) {
+	return item + 10;
+});
+// console.log( arr );
+// console.log( arr2 );
+
+
+/**
+ *  other usefull methods
+ */
+
+// find, findIndex
+// reduce, reduceRight
+// lastIndexOf
+// fill
+// concat, join
+// sort
+// every, some
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+/**
+ *  typeof
+ */
+
+// console.log(typeof arr);
---
+++ src/03.objects.js
@@ -0,0 +1,125 @@
+/**
+ *  Objects
+ */
+
+
+// object literal
+var me = {
+	firstName: 'Andrei',
+	lastName : 'Pfeiffer',
+	age: 34,
+	skills: [
+		{ name: 'html', level: 8 },
+		{ name: 'css',  level: 7 },
+		{ name: 'js',   level: 6 },
+	],
+	getAge: function() {
+		return this.age;
+	}
+};
+
+
+/**
+ *  usage
+ */
+
+// data structures (ES6 Set/Map)
+// code structure (methods)
+
+
+/**
+ *  property access
+ */
+
+// console.log( me.firstName );
+// console.log( me.skills[1].level );
+
+
+/**
+ *  properties with spaces
+ */
+
+// console.log( me['first Name'] );
+
+
+/**
+ *  properties as expressions
+ */
+var name = 'Name';
+// console.log( me['first ' + name] );
+
+
+/**
+ *  enhanced literals (ES6)
+ */
+
+
+/**
+ *  delete property
+ */
+
+// delete me.skills;
+// console.log(me);
+
+
+/**
+ *  property descriptors
+ */
+
+// console.log( Object.getOwnPropertyDescriptor(me, 'age') );
+// Object.defineProperty(me, 'age', {
+// 	writable: false
+// });
+
+
+/**
+ *  freezing (seal, preventExtensions)
+ */
+
+// Object.freeze(me);
+// me.age = 1;
+// me.city = 'Timisoara';
+// delete me.skills;
+// console.log( Object.isFrozen(me) );
+
+
+/**
+ *  iterations
+ */
+
+// console.log( Object.keys(me) );
+Object.keys(me).forEach(function(item) {
+// 	console.log(item);
+});
+
+var you = Object.create(me);
+you.firstName = 'Gigel';
+you.lastName = 'Popescu';
+for (var prop in you) {
+	// console.log(item);
+}
+
+
+/**
+ *  JSON - JavaScript Object Notation
+ */
+
+var data = { name: 'Andrei', age: 34 };
+// console.log( JSON.stringify( data ) );
+var json = '{ "name": "Andrei", "age": 34 }';
+// console.log( JSON.parse( json ) );
+
+
+/**
+ *  null
+ */
+
+// removes reference, clears memory
+// me = null;
+
+
+/**
+ *  typeof
+ */
+
+// console.log(typeof me);
