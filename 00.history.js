efd8052ac862e25005f2c6322a0120cfaec7008b
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-04-09 23:08:42 +0300

    Add history file

---
+++ 00.history.js
@@ -0,0 +1,50 @@
+1995 - Mocha (by Netscape, during May)
+     - LiveScript (september)
+     - JavaScript (december)
+     - DOM (level 0, non standard)
+
+1996 - JScript (Microsoft, Internet Explorer)
+     - ECMA (European Computer Manufacturers Association)
+
+1997 - ES1 ("ECMAScript: A general purpose, cross-platform programming language")
+
+1998 - ES2 (editorial changes, to conform to standard)
+     - DOM (level 1, W3C standard)
+
+1999 - ES3 (RexExp, try, catch, etc)
+
+2000
+
+2001
+
+2002
+
+2003
+
+2004
+
+2005
+
+2006
+
+2007
+
+2008 - ES4 (abandoned: classes, modules, static type, generators, iterators, destructuring)
+
+2009 - ES5 (aka "ES3.1": strict mode, JSON, added methods on existing prototypes)
+
+2010
+
+2011
+
+2012
+
+2013
+
+2014
+
+2015 - ES6 (aka "ES2015": classes, modules, generators, iterators, arrow functions, collections, promises, proxies, reflection)
+
+2016 - ES7 (aka "ES2016": array.includes, exponentiator operator)
+
+2017 - ES2017 (wip)
